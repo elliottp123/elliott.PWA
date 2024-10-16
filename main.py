44a0484901460b6d76800c5c9047fb8933301475
Elliott.P <124545912+elliottp123@users.noreply.github.com>
2024-10-16 02:01:17 +0000

    working website! about needs centering

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
 import database_manager as dbHandler
 
 app = Flask(__name__)
 
 @app.route('/index.html', methods=['GET'])
 @app.route('/', methods=['POST', 'GET'])
+
 def index():
    data = dbHandler.listExtension()
-   return render_template('/index.html', content=data)
+   print("inx route accessed")
+   return render_template('index.html', content=data)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+   print("Add route accessed")
+   return render_template('add.html')  # or just 'add.html' if it's in the main templates folder
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=5000)
+   app.run(debug=True, host='0.0.0.0', port=5000)
