53518eb677f68b04eef182ab1299b776a8f3ffd0
Elliott.P <124545912+elliottp123@users.noreply.github.com>
2024-10-17 22:41:17 +0000

    everything work yay

--- database_manager.py
+++ database_manager.py
@@ -1,9 +1,15 @@
 import sqlite3 as sql
 
 def listExtension():
-  con = sql.connect(".database/data_source.db") #!!!!
-  cur = con.cursor()
-  data = cur.execute('SELECT * FROM extension').fetchall()
-  con.close()
-  return data
-  
+    con = sql.connect(".database/data_source.db")  # Make sure this path is correct
+    cur = con.cursor()
+    data = cur.execute('SELECT * FROM extension').fetchall()
+    con.close()
+    return data
+
+def insertContact(email, name):
+    con = sql.connect(".database/data_source.db")  # Make sure this path is correct
+    cur = con.cursor()
+    cur.execute("INSERT INTO contact_list (email,name) VALUES (?,?)", (email,name))
+    con.commit()
+    con.close()
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import database_manager as dbHandler
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
 @app.route('/index.html', methods=['GET'])
-@app.route('/', methods=['POST', 'GET'])
-
 def index():
-   data = dbHandler.listExtension()
-   print("inx route accessed")
-   return render_template('index.html', content=data)
+    data = dbHandler.listExtension()
+    print("index route accessed")
+    return render_template('index.html', content=data)
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/add', methods=['POST', 'GET'])
 def add():
-   print("Add route accessed")
-   return render_template('add.html')  # or just 'add.html' if it's in the main templates folder
+    if request.method == 'POST':
+        email = request.form['email']
+        name = request.form['name']
+        dbHandler.insertContact(email, name)
+        print(f"Contact added: {email}, {name}")
+        return redirect(url_for('index'))
+    else:
+        print("add route accessed - GET")
+        return render_template('add.html')
 
 if __name__ == '__main__':
-   app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
