992e250885d45c07ba1106321d4e4f8774bd4a83
Elliott.P <124545912+elliottp123@users.noreply.github.com>
2024-10-20 09:43:54 +0000

    trying javascript

--- .workingDocuments/index.js
+++ .workingDocuments/index.js
@@ -1,19 +1,39 @@
-const spawn = require("child_process").spawn;
-//spawn('python',["NodeJS_PWA_Programming_For_The_Web_Task_Template/.workingDocuments/database_manager.py", arg1, arg2, ...])
-const pythonProcess = spawn("python", ["database_manager.py"]);
-console.log("it happened?")
-// Insert additional backend js above the express server configuration
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(".database/data_source.db");
 
-const express = require("express");
-const path = require("path");
-const app = express();
-app.use(express.static(path.join(__dirname, "public")));
+let myString = "[\n";
+db.all("SELECT * FROM extension", function (err, rows) {
+  let myCounter = 0;
+  rows.forEach(function (row) {
+    // for debugging
+    // console.log(row.extID + ": " + row.name + ": " + row.hyperlink + ": " + row.about + ": " + row.image + ": " + row.language);
+    myString =
+      myString +
+      '{\n"extID":' +
+      row.extID +
+      ',\n"name":"' +
+      row.name +
+      '",\n"hyperlink":"' +
+      row.hyperlink +
+      '",\n"about":"' +
+      row.about +
+      '",\n"image":"' +
+      row.image +
+      '",\n"language":"' +
+      row.language;
+    myCounter++;
+    if (myCounter == rows.length) {
+      myString = myString + '"\n}\n';
+    } else {
+      myString = myString + '"\n},\n';
+    }
+  });
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index/index.html"));
+  // console.log(myString);
+  var fs = require("fs");
+  fs.writeFile("public/frontEndData.json", myString + "]", function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
-app.listen(5000, () =>
-  console.log(
-    "Server is running on Port 5000, visit http://localhost:5000/ or http://127.0.0.1:5000 to access your website"
-  )
-);
