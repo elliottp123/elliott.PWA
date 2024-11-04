fb6656d57fffae014bc9b46ed8ce1371443f2694
Elliott.P <124545912+elliottp123@users.noreply.github.com>
2024-11-04 02:54:29 +0000

    lost cause last try with aigit add -A!

--- index.js
+++ index.js
@@ -45,11 +45,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
-app.listen(5000, () =>
+app.listen(3000, () =>
   console.log(
-    "Server is running on Port 5000, visit http://localhost:5000/ or http://127.0.0.1:5000 to access your website"
+    "Server is running on Port 5000, visit http://localhost:3000/ or http://127.0.0.1:3000 to access your website"
   )
 );
 
